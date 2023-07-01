09420e3f0e4e32b624a99966e65878bf691b263c
Ashraful Alim <33482997+Ashraful-alim@users.noreply.github.com>
2023-07-01 23:02:33 +0600

    Create 6. string.py

---
+++ 6. string.py
@@ -0,0 +1,17 @@
+# Problem :
+
+# Create a Python program that takes a user input string and performs the following string operations:
+# Prints the length of the string.
+# Prints the string in uppercase.
+
+# Solution :
+
+input_string = input("Enter a string: ")
+
+# Length of the string
+length = len(input_string)
+print("Length of the string:", length)
+
+# Uppercase
+uppercase = input_string.upper()
+print("Uppercase:", uppercase)
