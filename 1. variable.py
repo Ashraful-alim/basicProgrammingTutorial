93b1f536b628a5fa1729336770584b4b4d163e09
Ashraful Alim <33482997+Ashraful-alim@users.noreply.github.com>
2023-07-01 22:17:58 +0600

    Created 1. variable.py

---
+++ 1. variable.py
@@ -0,0 +1,13 @@
+# Problem :
+# Create a Python program that calculates the area of a rectangle. 
+# The program should prompt the user to enter the length and width of the rectangle, 
+# store the values in variables, and then calculate and display the area.
+
+# Answer :
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+
+print("The area of the rectangle is:", area)
